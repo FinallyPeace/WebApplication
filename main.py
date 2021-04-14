2234bb8d1be81c65fe0aea1ab5e0c5dbd173f4d0
FinallyPeace <ll7856l@gmail.com>
2021-04-14 23:30:20 +0800

    practice flask

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
-from flask import Flask,render_template,url_for,redirect
+from flask import Flask, render_template, url_for, redirect
+
 app = Flask(__name__)
 
-@app.route("/")
-@app.route("/<int:num>")
-def hello(name = None):
-    if name == None:
-        return "<h1 style='color:red;'>首頁</h1>"
-    return "<h1>第二層 <span style='color:blue;'>"+ name + "</span></h1>"
+# @app.route("/")
+# @app.route("/<name>")
+# def hello(name = None):
+#     if name == None:
+#         return "<h1 style='color:red;'>首頁</h1>"
+#     return "<h1>第二層 <span style='color:blue;'>"+ name + "</span></h1>"
 
 # @app.route('/hello/world', methods=['POST', 'GET'])
 # def postGet():
 #     return 'Post and Get'
 
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/jp")
+def jp():
+    return render_template("jp.html")
+
+@app.route("/test/<int:n>")
+def num(n):
+    return render_template("test.html", n=n)
