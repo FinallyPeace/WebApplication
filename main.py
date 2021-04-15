ed0e3a7a5b89e09b5d8e1a9fb16fa8664042ba35
FinallyPeace <ll7856l@gmail.com>
2021-04-15 15:43:50 +0800

    practice url_for(static, filname)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for, redirect
 
-app = Flask(__name__)
+app = Flask(__name__,static_folder="../static/")
 
 # @app.route("/")
 # @app.route("/<name>")
@@ -13,7 +13,9 @@ app = Flask(__name__)
 # def postGet():
 #     return 'Post and Get'
 
-
+# @app.route("/test/<int:n>")
+# def num(n):
+#     return render_template("test.html", n=n)
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -23,6 +25,6 @@ def index():
 def jp():
     return render_template("jp.html")
 
-@app.route("/test/<int:n>")
-def num(n):
-    return render_template("test.html", n=n)
+@app.route("/jp-list")
+def jpList():
+    return render_template("jp-list.html")
